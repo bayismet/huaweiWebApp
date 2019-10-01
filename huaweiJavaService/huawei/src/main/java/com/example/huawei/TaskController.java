af93c80630a307928d828d9a49299e2c170b296e
İsmet Bayramoğlu <iismetbayramoglu@gmail.com>
2019-10-01 14:34:03 +0200

    Update TaskController.java

--- huaweiJavaService/huawei/src/main/java/com/example/huawei/TaskController.java
+++ huaweiJavaService/huawei/src/main/java/com/example/huawei/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "null")
 public class TaskController {
     @Autowired
     public UserService userService;
