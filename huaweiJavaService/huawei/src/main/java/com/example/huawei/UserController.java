d373543b9f27d00d9c8326a85cad6cb18e426791
İsmet Bayramoğlu <iismetbayramoglu@gmail.com>
2019-10-01 14:34:23 +0200

    Update UserController.java

--- huaweiJavaService/huawei/src/main/java/com/example/huawei/UserController.java
+++ huaweiJavaService/huawei/src/main/java/com/example/huawei/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * UserController
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "null")
 public class UserController {
 
     @Autowired
